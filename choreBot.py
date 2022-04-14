3fa36483fabb404a5b775942430a7251af9f64d2
cschlicht <cameronschlicht7@gmail.com>
2022-04-13 19:54:34 -0700

    names

--- choreBot.py
+++ choreBot.py
@@ -5,4 +5,16 @@
 # Dictionary initialization
 chores = {0:"weekly", 1:"dishler", 2:"warden", 3:"counter", 4:"livingRoom", 5:"trash", 6:"unload"}
 
+isaac = 0
+adam = 1
+benji = 2
+arya = 3
+troy = 4
+steve = 5
+cameron = 6
+
 # Chore algo
+
+
+
+
